fa66e9005a07b32e02519d792b7a5972946f7a47
EC2 Default User <ec2-user@ip-172-31-38-104.us-west-2.compute.internal>
2023-04-18 17:48:37 +0000

    Add search and layout to book viewer project

--- rb175/book_viewer/book_viewer.rb
+++ rb175/book_viewer/book_viewer.rb
@@ -2,17 +2,68 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+
+  def highlight(line, query)
+    line.gsub(query, %(<strong>#{query}</strong>))
+  end
+
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |paragraph, idx|
+      "<p id='p#{idx}'>#{paragraph}</p>"
+    end.join
+  end
+
+  def each_chapter
+    @contents.each_with_index do |chap_name, idx|
+      chap_num = idx + 1
+      chap_contents = File.read("data/chp#{chap_num}.txt")
+      yield chap_num, chap_name, chap_contents
+    end
+  end
+
+  def chapters_including(query)
+    results = []
+    return results unless query
+
+    each_chapter do |chap_num, chap_name, chap_contents|
+      matches = {}
+      chap_contents.split("\n\n").each_with_index do |line, idx|
+        matches[idx] = line if line.include?(query)
+      end  
+      results << {chap_num: chap_num, chap_name: chap_name, lines: matches} if matches.any?
+    end
+    results
+  end
+end
+
+not_found do
+  "That page was not found!"
+  redirect "/"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.readlines("data/chp1.txt")
+get "/chapters/:number" do
+  num = params[:number].to_i
+  chap = File.readlines("data/toc.txt")[num - 1]
+
+  @title = "Chapter #{num}: #{chap}"
+  @chapter = File.read("data/chp#{num}.txt")
 
   erb :chapter
 end
+
+get "/search" do
+  @results = chapters_including(params[:query])
+  erb :search
+end
+
